356f36e147084e2d354fdd39ed4b7af0667a2d58
TranTam <tamtt1@firegroup.io>
2023-02-27 00:43:09 +0700

    mist update

--- mu-plugins/university-post-types.php
+++ mu-plugins/university-post-types.php
@@ -2,6 +2,10 @@
 function university_posts_types()
 {
     register_post_type('event', array(
+        'show_in_rest' => true,
+        'supports' => array(
+            'title', 'editor', 'excerpt'
+        ),
         'rewrite' => array('slug' => 'events'),
         'has_archive' => true,
         'public' => true,
