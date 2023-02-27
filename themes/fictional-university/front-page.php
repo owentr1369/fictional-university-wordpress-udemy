f590ed812c8e5b6d2a24158a53a2926c3421a99c
TranTam <tamtt1@firegroup.io>
2023-02-27 17:15:17 +0700

    custom field and past events

--- themes/fictional-university/front-page.php
+++ themes/fictional-university/front-page.php
@@ -21,7 +21,7 @@
                 'posts_per_page' => -1,
                 'post_type' => 'event',
                 'meta_key' => 'event_date',
-                'orderby' => 'meta_value_num',
+                'orderby' => 'event_date',
                 'order' => 'ASC',
                 'meta_query' => array(
                     array(
@@ -37,8 +37,14 @@
 
                 <div class="event-summary">
                     <a class="event-summary__date t-center" href="#">
-                        <span class="event-summary__month"><?php the_time('M') ?></span>
-                        <span class="event-summary__day"><?php the_time('d') ?></span>
+                        <span class="event-summary__month">
+                            <?php
+                            $eventDate = new DateTime(get_field('event_date'));
+                            echo    $eventDate->format('M')
+                            ?></span>
+                        <span class="event-summary__day">
+                            <?php
+                            echo    $eventDate->format('d') ?></span>
                     </a>
                     <div class="event-summary__content">
                         <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
