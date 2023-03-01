040fd98663b453c9aabf0ffca016a3985b2c93bb
TranTam <tamtt1@firegroup.io>
2023-03-01 11:38:46 +0700

    manual crop plugin

--- themes/fictional-university/single-professor.php
+++ themes/fictional-university/single-professor.php
@@ -16,7 +16,7 @@ while (have_posts()) {
         <div class="generic-content">
             <div class="row group">
                 <div class="one-third">
-                    <?php the_post_thumbnail() ?>
+                    <?php the_post_thumbnail('professorPortrait') ?>
                 </div>
                 <div class="two-third">
                     <?php the_content() ?>
